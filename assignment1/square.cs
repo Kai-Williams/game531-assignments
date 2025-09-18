ba34d930da0899f454ffec71ac8bf9dba538a147
Kai <kaiemwill28@gmail.com>
2025-09-18 14:15:38 -0400

    First commit

---
+++ assignment1/square.cs
@@ -0,0 +1,126 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using System.Drawing;
+
+namespace FirstOpenTK
+{
+    public class Game : GameWindow
+    {
+        private int _vbo;
+        private int _vao;
+        private int _ebo;
+        private int _shader;
+
+        public Game(GameWindowSettings gws, NativeWindowSettings nws) : base(gws, nws) { }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+            GL.ClearColor(0.12f, 0.13f, 0.16f, 1f);
+
+            float[] vertices =
+            {
+                 0.5f,  0.5f, 0.0f,
+                 0.5f, -0.5f, 0.0f,
+                -0.5f, -0.5f, 0.0f,
+                -0.5f,  0.5f, 0.0f
+            };
+
+            uint[] indices = { 0, 1, 3, 1, 2, 3 };
+
+            _vbo = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vbo);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+
+            _vao = GL.GenVertexArray();
+            GL.BindVertexArray(_vao);
+
+            _ebo = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, _ebo);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(uint), indices, BufferUsageHint.StaticDraw);
+
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+            const string vert = @"#version 330 core
+layout (location = 0) in vec3 aPos;
+void main() {
+    gl_Position = vec4(aPos, 1.0);
+}";
+            const string frag = @"#version 330 core
+out vec4 FragColor;
+void main() {
+    FragColor = vec4(0.6, 0.2, 0.8, 1.0);
+}";
+
+            _shader = CreateProgram(vert, frag);
+
+            GL.BindVertexArray(0);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+            if (KeyboardState.IsKeyDown(Keys.Escape))
+                Close();
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            base.OnRenderFrame(args);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.UseProgram(_shader);
+            GL.BindVertexArray(_vao);
+            GL.DrawElements(PrimitiveType.Triangles, 6, DrawElementsType.UnsignedInt, 0);
+            SwapBuffers();
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+            GL.Viewport(0, 0, Size.X, Size.Y);
+        }
+
+        protected override void OnUnload()
+        {
+            base.OnUnload();
+            GL.DeleteBuffer(_vbo);
+            GL.DeleteBuffer(_ebo);
+            GL.DeleteVertexArray(_vao);
+            GL.DeleteProgram(_shader);
+        }
+
+        private static int CreateProgram(string vertexSrc, string fragmentSrc)
+        {
+            int vs = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(vs, vertexSrc);
+            GL.CompileShader(vs);
+            GL.GetShader(vs, ShaderParameter.CompileStatus, out int vOk);
+            if (vOk == 0) throw new System.Exception(GL.GetShaderInfoLog(vs));
+
+            int fs = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(fs, fragmentSrc);
+            GL.CompileShader(fs);
+            GL.GetShader(fs, ShaderParameter.CompileStatus, out int fOk);
+            if (fOk == 0) throw new System.Exception(GL.GetShaderInfoLog(fs));
+
+            int prog = GL.CreateProgram();
+            GL.AttachShader(prog, vs);
+            GL.AttachShader(prog, fs);
+            GL.LinkProgram(prog);
+            GL.GetProgram(prog, GetProgramParameterName.LinkStatus, out int pOk);
+            if (pOk == 0) throw new System.Exception(GL.GetProgramInfoLog(prog));
+
+            GL.DetachShader(prog, vs);
+            GL.DetachShader(prog, fs);
+            GL.DeleteShader(vs);
+            GL.DeleteShader(fs);
+            return prog;
+        }
+    }
+}
